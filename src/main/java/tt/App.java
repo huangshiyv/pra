d3600934c1505548b4dfd971d8385fa90e73ec94
huang shiyu <huangshiyv@gmail.com>
2019-03-27 00:54:59 +0100

    oo

---
+++ src/main/java/tt/App.java
@@ -0,0 +1,23 @@
+package tt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public boolean isPalindr(String input)
+    {
+        return false;
+    }
+
+    private  String reveerse(String input)
+    {
+        return input+"1";
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
